279d27cf5025244bf8f4a9861b6da99d225ed303
Brilly <ASUS@LAPTOP-BEUE1DIV>
2022-11-07 17:07:20 +0700

    menambahkan isi var add note

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -7,17 +7,17 @@ using System.Threading.Tasks;
 namespace Singly_linked_list
 {
     //each nude consist of the information part and link to the next nude
-    class nude
+    class node
     {
         public int rollNumber;
         public string name;
-        public nude next;
+        public node next;
     }
 
     class list
     {
-        nude START;
-        public List()
+        node START;
+        public list()
         {
             START = null;
         }
@@ -25,7 +25,27 @@ namespace Singly_linked_list
         {
             int rollNo;
             string nm;
-            Console.Write();
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll name of the student: ");
+            nm = Console.ReadLine();
+            node newnode = new node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            //if the node to be inserted is the first node
+            if ((START == null || rollNo <= START.rollNumber))
+            {
+                if((START != null) && (rollNo == START.rollNumber))
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                newnode.next = START;
+                START = newnode;
+                return;
+            }
+            
+
         }
     }
     class Program
