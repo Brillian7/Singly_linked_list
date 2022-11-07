cbcf8365fcf8f5baa46cc92e89abdf39f2407207
Brilly <ASUS@LAPTOP-BEUE1DIV>
2022-11-07 16:48:39 +0700

    Add project files.

---
+++ Singly_linked_list/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singly_linked_list
+{
+    //each nude consist of the information part and link to the next nude
+    class nude
+    {
+        public int rollNumber;
+        public string name;
+        public nude next;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
